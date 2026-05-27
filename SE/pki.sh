8df4321b95f5068838c20591532d440175b0d494
semvlu <74308718+semvlu@users.noreply.github.com>
2026-05-27 13:56:17 +0800

    Update pki.sh

--- SE/pki.sh
+++ SE/pki.sh
@@ -30,3 +30,29 @@ vault write pki/roles/kube-certs \
     
 # Issue cert for www.example.com
 vault write pki/issue/example-dot-com common_name=www.example.com
+
+# --- Kubernetes Root CA exp issue ---
+# GW port FW 443 fail fix: CA issuer 
+vault secrets tune -max-lease-ttl=87600h pki # 10 yr
+
+# Gen Root CA
+vault write pki/root/generate/internal \
+    common_name="cluster.local" \
+    issuer_name="root-CA" \
+    ttl=87600h
+
+# Set default issuer
+vault write pki/config/issuers default="<ISSUER-UUID>"
+
+# Allow subdomains
+vault write pki/roles/kube-certs \
+    allowed_domains="local" \
+    allow_subdomains=true \
+    issuer_ref="default"
+
+# Force cert-manager to fetch new cert
+kubectl delete certificate gateway-tls-cert -n envoy-gateway-system
+kubectl apply -f gateway-cert.yaml
+
+# Delete old Root CA
+vault delete pki/issuer/<OLD-ISSUER-UUID>
